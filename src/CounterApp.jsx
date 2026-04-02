2e1eb8a20e5e151fad70c3e48a544cc314e54be5
hJun-KR <h0ngjunp.park@gmail.com>
2026-04-02 09:54:20 +0900

    feat: 추가 +1 새로 추가

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 // import './App.css'
 
 function CounterApp() {
+  // count : 상태 값, 이전 상태 값
+  // setCount() : 상태를 바꾸는 함수, 반드시 애로 바꿔야 함
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button
+    <h1>{count}</h1>
+    <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)}>
         Count is {count}
-      </button>
+      </button> */}
     </>
   )
 }
