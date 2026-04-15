e9d79d0520568816cd4b924a745f6d23aba00843
hJun-KR <h0ngjunp.park@gmail.com>
2026-04-15 11:10:11 +0900

    ref: Button -> Commponent로 변환

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import'./todolist.css'
 import TodoItemEmpty from './components/TodoItemEmpty'
+import Button from './components/Button'
 
 function TodoListApp() {
   return (
@@ -8,7 +9,7 @@ function TodoListApp() {
 			<h1 className='todo__title'>HyeonDo List</h1>
 			<form className='todo__form'>
 				<input type="text" className='todo__input'  placeholder='할 일을 입력하세요.'/>
-				<button type="submit" className='todo__button todo__button--add'>Add</button>
+				<Button type="submit" className='todo__button todo__button--add'>Add</Button>
 			</form>
 			<ul className='todo__list'>
 				<TodoItemEmpty />
@@ -16,8 +17,8 @@ function TodoListApp() {
 				<li className='todo__item todo__item--complete'>
 					<input type="checkbox" id="chk-1" className='todo__check' />
 					<label htmlFor="chk-1" className='todo__label'>지각하기</label>
-					<button type="button" className='todo__button todo__button--edit'>✏️</button>
-					<button type="button" className='todo__button todo__button--delete'>❌</button>
+					<Button type="button" className='todo__button todo__button--edit'>✏️</Button>
+					<Button type="button" className='todo__button todo__button--delete'>❌</Button>
 				</li>				
 			</ul> 
     </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button(props) {
+    const { children, ...rest } = props;
+    return (
+        <button {...rest}>{children}</button>
+
+    )
+}
+
+export default Button
